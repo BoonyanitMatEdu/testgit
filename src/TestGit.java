895943fb7908521e695e47eecf1bb246e1054e8a
boonyanit <boonyanit.mat@mahidol.edu>
2018-03-27 17:59:39 +0700

    Revert "Say nothing"
    This reverts commit ef16d61

--- src/TestGit.java
+++ src/TestGit.java
@@ -2,5 +2,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Ho");
+        System.out.println("Yes");
     }
 }
