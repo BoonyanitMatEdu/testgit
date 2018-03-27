ef16d613773574c183183825f700287bfb55909f
boonyanit <boonyanit.mat@mahidol.edu>
2018-03-27 17:58:56 +0700

    Say nothing

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,7 +1,5 @@
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("Hi");
-        System.out.println("Ho");
-        System.out.println("Yes");
+
     }
 }
