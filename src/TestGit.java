c1c23f911c2f8c975945565e36f6cc81f2dac82a
boonyanit <boonyanit.mat@mahidol.edu>
2018-03-27 17:56:11 +0700

    Delete "Yes"

--- src/TestGit.java
+++ src/TestGit.java
@@ -2,7 +2,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Ho");
-        System.out.println("Yes");
         System.out.println("No");
     }
 }
